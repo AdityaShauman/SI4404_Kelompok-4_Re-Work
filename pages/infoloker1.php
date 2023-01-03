ccc36e6baf5b446f847f60926dfd88ce4fecd381
Ashari12 <rahmahdane412@gmail.com>
2023-01-03 07:53:45 +0700

    Revert "Merge branch 'main' of https://github.com/AdityaShauman/SI4404_Kelompok-4_Re-Work"
    This reverts commit 4f19885e8618546bb7f22dc8bb262f71a1986c80, reversing
    changes made to df9a93de1b64a101cd598065f6b2ab46e48a9cad.

--- pages/infoloker1.php
+++ pages/infoloker1.php
@@ -1,22 +1,28 @@
-<?php
-require "/xampp/htdocs/Re-Work/config/db_connector.php";
-$title = "Loker";
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Loker</title>
+    <title>Projects - Brand</title>
     <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800&amp;display=swap">
 </head>
 
 <body>
-<?php include "Navbar.php" ?>
+    <nav class="navbar navbar-light navbar-expand-md sticky-top navbar-shrink py-3" id="mainNav">
+        <div class="container"><a class="navbar-brand d-flex align-items-center" href="/"></a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navcol-1"><img src="../assets/img/LOGO.png" width="86" height="35">
+                <ul class="navbar-nav mx-auto">
+                    <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="Materi.php">Materi</a></li>
+                    <li class="nav-item"><a class="nav-link" href="pelatihan.php">Pelatihan</a></li>
+                    <li class="nav-item"><a class="nav-link" href="infoloker2.php">Info Loker</a></li>
+                    <li class="nav-item"><a class="nav-link" href="contacts.php">Contacts</a></li>
+                </ul><a class="btn btn-warning shadow" role="button" href="signup.php">Sign up</a>
+            </div>
+        </div>
+    </nav>
     <section class="py-5">
         <div class="container py-5">
             <div class="row mb-5">
