4d3510f6bee790602d6f7270f64ff7973d81928f
AdityaShauman <aditya.shauman@gmail.com>
2023-01-07 14:56:45 +0700

    model dan controller

---
+++ laravel/app/Models/Loker.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Loker extends Model
+{
+    public $table = 'loker';
+    use HasFactory;
+
+    protected $fillable = [
+        'nama',
+        'deksripsi',
+        'foto',
+        'link',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime'
+    ];
+}
